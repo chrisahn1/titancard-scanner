61289f9e1306ec5cbf896b98dd1e0c374c2975a6
Jacob <jcbpowell12@gmail.com>
2018-10-19 09:47:37 +0000

    Delete GUI-main.py

--- gui/GUI-main.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-class Window:
-    def __init__(self, window, title):
-        self.window = window
-        self.window.title(title)
-        self.window.mainloop()
-
-Window(tkinter.Tk(), "Titan Card Scanner")
