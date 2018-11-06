2e47ea3091624e81d921654a4b98e66dce45d476
Jacob <jcbpowell12@gmail.com>
2018-11-06 08:03:03 -0100

    Fixed merge conflicts

--- gui/Video.py
+++ gui/Video.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2
 
 class Video:
@@ -21,27 +20,3 @@ class Video:
         if self.video_source.isOpened():
             self.video_source.release()
             cv2.destroyAllWindows()
-=======
-import cv2
-
-class Video:
-    def __init__(self, video_source):
-        self.video_source = video_source
-        if not self.video_source.isOpened():
-            raise ValueError("cant connect to video source", video_source)
-
-        self.width = video_source.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = video_source.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-    def update(self):
-        while True:
-            ret, frame = self.video_source.read()
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    def __del__(self):
-        if self.video_source.isOpened():
-            self.video_source.release()
-            cv2.destroyAllWindows()
->>>>>>> 28ac6228d3f02ed7fbaa1d93a48786ce6fa636da
