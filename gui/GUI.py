c7ce5a0dc4b6d7cdf48d3dd9b19efc7c9010f5ff
Jacob <jcbpowell12@gmail.com>
2018-11-06 08:02:34 -0100

    Fixed merge conflict

--- gui/GUI.py
+++ gui/GUI.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from tkinter import *
 import cv2
 from Video import Video
@@ -12,18 +11,3 @@ vid = Video(cap)
 
 #loop to keep video running
 vid.update()
-=======
-from tkinter import *
-import cv2
-from Video import Video
-# color = #00274C
-
-#defined video source
-cap = cv2.VideoCapture(0)
-
-#instantiate video object
-vid = Video(cap)
-
-#loop to keep video running
-vid.update()
->>>>>>> 28ac6228d3f02ed7fbaa1d93a48786ce6fa636da
